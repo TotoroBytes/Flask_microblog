a2cee336b2c98da6a9499239bcd22db276ea8b63
TotoroBytes <121270358+TotoroBytes@users.noreply.github.com>
2026-03-25 04:26:00 +0100

    Committing changes

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -1,1 +1,8 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+
+@app.shell_context_processor
+def make_shell_content():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
